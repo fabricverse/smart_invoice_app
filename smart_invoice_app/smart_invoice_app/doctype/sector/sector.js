65a8e262b11e90e1ce68f41045d0973c896eaa74
dawoodjee <adam.daveed@gmail.com>
2024-09-26 20:17:06 +0200

    added dependant docs

---
+++ smart_invoice_app/smart_invoice_app/doctype/sector/sector.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2024, Bantoo and Partners and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Sector", {
+// 	refresh(frm) {
+
+// 	},
+// });
