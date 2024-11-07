d338159fa42b93962eaed13a5a15ea6c92608af3
Patrick <patrick@thebantoo.com>
2024-11-07 12:59:54 +0200

    Add Doc Prints, Smart Invoice Workspace and Reports

---
+++ smart_invoice_app/smart_invoice_app/report/si_trial_balance/si_trial_balance.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2024, Bantoo and Partners and contributors
+# For license information, please see license.txt
+
+# import frappe
+
+
+def execute(filters=None):
+	columns, data = [], []
+	return columns, data
