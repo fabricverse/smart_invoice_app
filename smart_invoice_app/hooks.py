416de9a239deba87157335eed2866265d98ed31c
Adam Dawoodjee <adam@Adams-MacBook-Pro.local>
2025-01-03 23:49:20 +0200

    add save_sale offline, test_connection in api.py -

--- smart_invoice_app/hooks.py
+++ smart_invoice_app/hooks.py
@@ -158,6 +158,7 @@ doc_events = {
     },
     "Sales Invoice": {
         "on_update": "smart_invoice_app.app.save_invoice_api",
+        # "after_insert": "smart_invoice_app.app.save_invoice_api",
         "on_submit": "smart_invoice_app.app.save_invoice_api",
         "on_cancel": "smart_invoice_app.app.delete_qr_code_file"
     },
